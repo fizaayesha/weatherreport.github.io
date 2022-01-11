ea2c3b9235f0627fa7e8029e9ec506dfa088d792
Ayesha <fizaayesha696@gmail.com>
2022-01-11 16:30:04 +1200

    Weather Report
    Weather Report

---
+++ script.js
@@ -0,0 +1,48 @@
+let weather = {
+    "apiKey": "a3c12f06328f5fd481d509a8858d6437",
+    
+    fetchWeather: function (city) {
+        fetch(
+            "http://api.openweathermap.org/data/2.5/weather?q="
+            + city +
+            "&units=metric&appid="
+            + this.apiKey
+            // ,
+            //  {
+            // mode: 'no-cors'
+            // }
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        // const { icon, desciption } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        // console.log(name, icon, desciption, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        // document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "04n.png";
+        // document.querySelector(".description").innerText = desciption;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/random/900x700/?" + name + "')"
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if((event.key) == "Enter"){
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Denver");
